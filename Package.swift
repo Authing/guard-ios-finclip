a4793380ace2f4f16d09cbf07b180f770b653ae3
jnMars <563438383@qq.com>
2022-04-19 18:12:26 +0800

    Fix - checksum

--- Package.swift
+++ Package.swift
@@ -17,8 +17,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "GuardFinClipExtender",
-            url: "https://github.com/Authing/guard-ios-finclip/releases/download/0.0.1/GuardFinClipExtender.xcframework.zip",
-            checksum: "125b6d3cb9500c51bec746aecc21cb2e2c2b7c0a7d323c386cdca11b6d0991b9"
+            url: "https://github.com/Authing/guard-ios-finclip/releases/download/1.0.0/GuardFinClipExtender.xcframework.zip",
+            checksum: "516969a2ef172bc3c35ae99cb10a65ae94728f458bc9aacdfe254b47d0f2101a"
         )
     ]
 )
